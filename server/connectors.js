c80f5c1b9fcb5f651965d58220c300d25a9fb88d
LuciusCao <lucius.cao@gmail.com>
2016-12-16 00:24:31 +0800

    5 seed authors and 1 post for each

--- server/connectors.js
+++ server/connectors.js
@@ -1,5 +1,6 @@
 import Mongoose from 'mongoose';
 import casual from 'casual';
+import _ from 'lodash';
 
 Mongoose.connect('mongodb://localhost/blogs');
 
@@ -77,115 +78,36 @@ const Comment = Mongoose.model('comments', CommentSchema);
 
 // create seed data
 casual.seed(0);
-
-// const author1 = new Author({
-//   name: casual.name,
-//   email: casual.email,
-//   posts: [],
-//   comments: [],
-// });
-// author1.save((e) => {
-//   if (e) {
-//     throw e;
-//   } else {
-//     console.log('author1 created');
-//     const post1 = new Post({
-//       title: casual.title,
-//       category: 'design',
-//       content: casual.sentences(100),
-//       views: Math.floor(Math.random() * 100),
-//       author: author1._id,
-//       comments: [],
-//     });
-//     post1.save((e) => {
-//       if (e) {
-//         throw e;
-//       } else {
-//         console.log('post 1 created');
-//       };
-//     });
-//   };
-// });
-
-// Author.count({}, (e, r) => {
-//   if (e) {
-//     console.log('Error');
-//   } else if (r == 0) {
-//     Author.create({
-//       firstName: 'Lucius',
-//       lastName: 'Cao',
-//       posts: ['a post by lulu'],
-//     });
-//     Author.create({
-//       firstName: 'Ulleo',
-//       lastName: 'Wang',
-//       posts: ['fancy article', 'another fancy article'],
-//     });
-//     console.log('2 authors created');
-//   } else {
-//     console.log('authors already existed');
-//   }
-// });
-//
-// Post.count({}, (e, r) => {
-//   if (e) {
-//     console.log('Error');
-//   } else if (r == 0) {
-//     Post.create({
-//       title: 'a post by lulu',
-//       content: 'some content',
-//       views: 100,
-//       author: 'Lucius Cao',
-//       comments: ['this is the first comment', 'this is the second comment'],
-//     });
-//     Post.create({
-//       title: 'fancy article',
-//       content: 'some content here',
-//       views: 100,
-//       author: 'Ulleo Wang',
-//       comments: ['this is a comment'],
-//     });
-//     Post.create({
-//       title: 'another fancy article',
-//       content: 'some content here',
-//       views: 100,
-//       author: 'Ulleo Wang',
-//       comments: ['this is a comment'],
-//     });
-//     console.log('3 posts created');
-//   } else {
-//     console.log('posts already existed');
-//   }
-// });
-//
-// Comment.count({}, (e, r) => {
-//   if (e) {
-//     console.log('Error');
-//   } else if (r == 0) {
-//     Comment.create({
-//       post: 'a post by lulu',
-//       content: 'this is the first comment',
-//       author: 'Lucius Cao',
-//     });
-//     Comment.create({
-//       post: 'a post by lulu',
-//       content: 'this is the second comment',
-//       author: 'Ulleo Wang',
-//     });
-//     Comment.create({
-//       post: 'fancy article',
-//       content: 'this is a comment for fancy article',
-//       author: 'Lucius Cao',
-//     });
-//     Comment.create({
-//       post: 'another fancy article',
-//       content: 'this is a comment for another fancy article',
-//       author: 'Lucius Cao',
-//     });
-//     console.log('4 comments created');
-//   } else {
-//     console.log('comments already existed')
-//   }
-// });
+Author.count({}, (e, r) => {
+  if (e) {
+    throw e;
+  } else if (r == 0) {
+    _.times(5, () => {
+      return Author.create({
+        username: casual.username,
+        email: casual.email,
+        posts: [],
+        comments: []
+      }).then((author) => {
+        return Post.create({
+          title: casual.title,
+          category: ['design','technology','others'][casual.integer(0,2)],
+          content: casual.sentences(50),
+          views: casual.integer(0,1000),
+          author: author.id,
+          comments: []
+        }).then((post) => {
+          return Author.update(
+            {_id: post.author},
+            {$push: {posts: post._id}}
+          );
+        })
+      });
+    });
+    console.log('seed data created');
+  } else {
+    console.log('already existed');
+  }
+})
 
 export { Author, Post, Comment };
