3f8a6b33455b65e58352638b7385f0ffd9f306ca
LuciusCao <lucius.cao@gmail.com>
2016-12-25 14:00:44 +0800

    test for vim

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,8 +1,31 @@
 import { Author, Post, Comment } from './connectors';
 import casual from 'casual';
+import Mongoose from 'mongoose';
 
 const resolvers = {
   Query: {
+    getCurrentAuthor(root, args) {
+      return Author.findOne({ email: args.email }, (e, r) => {
+        if (e) {
+          throw new Error('error when get current author');
+        } else if (r) {
+          console.log(r.id);
+        } else {
+          throw new Error('author does not exist')
+        }
+      });
+    },
+    getMyPosts(root, args) {
+      return Post.find({ author: Mongoose.Types.ObjectId(context.id) }, (e, r) => {
+        if (e) {
+          throw new Error('error when get my posts');
+        } else if (r.lenth != 0) {
+          console.log(r);
+        } else {
+          throw new Error('no posts');
+        }
+      });
+    },
     author(root, args) {
       return {
         username: () => casual.username,
@@ -27,6 +50,15 @@ const resolvers = {
         username: args.username,
         email: args.email,
         posts: [],
+        comments: []
+      });
+    },
+    createPost(root, args, context) {
+      return Post.create({
+        title: args.title,
+        category: args.category,
+        content: args.content,
+        author: Mongoose.Types.ObjectId(context.authorId),
         comments: []
       });
     }
