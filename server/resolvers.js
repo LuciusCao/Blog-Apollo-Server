2ad75d7b1506a544960b6401d97d991d4f0b500b
LuciusCao <lucius.cao@gmail.com>
2016-12-26 14:38:07 +0800

    document getOnePost

--- server/resolvers.js
+++ server/resolvers.js
@@ -62,6 +62,9 @@ const resolvers = {
       }
     },
     getOnePost(root, args) {
+      /*
+      get one post's information via id. args.id is required
+      */
       return Post.findOne({_id: Mongoose.Types.ObjectId(args.id)}, (e, r) => {
         if (e) {
           throw new Error('getOnePost failed with args.id');
